6b80f131bd1f8f5c9b8f1046769b63768fafa8ae
eliyan-mu <eliyanmousazadeh@hilma.tech>
2024-10-30 17:10:20 +0200

    style

--- src/keyboard.jsx
+++ src/keyboard.jsx
@@ -3,10 +3,30 @@ import React from 'react'
 
 
 function MakeKeyboard(props){
+    const myStyle={
+        width:"600px",
+        hight:"50px",
+        display: "flex",
+        flexWrap: "wrap",
+        size:"50px"
+    }
+    const buttonStyle = {
+        fontSize: "30px",  
+        width: "50px",
+        height: "50px",
+        margin: "5px",
+        backgroundColor: "black",
+        color: "white",
+        border:"inset",
+        textAlign:"center",
+        justifyContent: "center",    
+        alignItems: "center"      
+
+    };
     return(
-        <div>
-   { props.arrayLetters.map((letter,index) => <button> {letter} </button>)}
-   </div> 
+        <div   style={myStyle}  className="keyboard" >
+            { props.arrayLetters.map((letter,index) => <button key={index} style={buttonStyle}> {letter} </button>)}
+        </div> 
    );
 }
 export default MakeKeyboard;
