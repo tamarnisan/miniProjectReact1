1de7429881145641e787f762e2e483e4f4faf3d5
tamar-nisan <tamarnisan@hilms.tech>
2024-10-30 17:07:45 +0200

    keyboard

--- src/keyboard.jsx
+++ src/keyboard.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 
 
 
-function MakeKeyboard(props){
-    return(
-        <div>
-   { props.arrayLetters.map((letter,index) => <button> {letter} </button>)}
-   </div> 
-   );
+function MakeKeyboard(props) {
+    function clickLetter(index) {
+        props.onKeyClicked((index) 
+        )
+    }
+    return (
+        <>
+            <div>{props.text} </div>
+            <div>
+                {props.arrayLetters.map((letter, index) => <button onClick={() => clickLetter(index)}> {letter} </button>)}
+            </div>
+        </>
+    );
 }
 export default MakeKeyboard;
